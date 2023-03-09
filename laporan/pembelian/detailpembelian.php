43ac3aa7a0a782b9212a96306e5758e917c246e6
nigelwilliam16 <s160419017@student.ubaya.ac.id>
2023-03-09 15:21:52 +0700

    detail pembelian

--- laporan/pembelian/detailpembelian.php
+++ laporan/pembelian/detailpembelian.php
@@ -9,24 +9,38 @@ if($conn->connect_error) {
 setlocale(LC_ALL, 'IND');
 
 extract($_POST);
-
-$sql = "SELECT nota_beli_product.id_nota_beli, nota_beli.tanggal, nota_beli.waktu, product.jenis, product.harga, 
-nota_beli_product.quantity, nota_beli_product.total FROM nota_beli INNER JOIN nota_beli_product ON nota_beli.id = nota_beli_product.id_nota_beli 
-INNER JOIN product ON product.id = nota_beli_product.id_product WHERE nota_beli_product.id_nota_beli = ?";
-
+$sql = "SELECT nota_beli.id, nota_beli.tanggal, nota_beli.waktu, SUM(nota_beli_product.quantity) AS jumlah_barang,
+SUM(nota_beli_product.total) AS total_pembelian, nota_beli.foto, nota_beli.id_cabang, cabang.nama_cabang, nota_beli.username, account.nama_depan, account.nama_belakang FROM nota_beli INNER JOIN nota_beli_product 
+ON nota_beli.id = nota_beli_product.id_nota_beli INNER JOIN cabang ON nota_beli.id_cabang = cabang.id 
+INNER JOIN account ON nota_beli.username = account.username WHERE nota_beli_product.id_nota_beli = ?";
 $stmt = $conn->prepare($sql);
+$stmt->bind_param("s",$id);
 $stmt->execute();
 $result = $stmt->get_result();
-$data=[];
 if($result->num_rows > 0) {
-  while($r=mysqli_fetch_assoc($result)) {    
-    $tahun = substr($r['tanggal'], 0,4);
-    $bulan = substr($r['tanggal'], 5,2);
-    $tanggal = substr($r['tanggal'], 8,2);
-    $r['tanggal'] = strftime( "%A %d %B %Y", mktime(0,0,0,$bulan,$tanggal,$tahun));
-    array_push($data,$r);
+  $r=mysqli_fetch_assoc($result);  
+  $tahun = substr($r['tanggal'], 0,4);
+  $bulan = substr($r['tanggal'], 5,2);
+  $tanggal = substr($r['tanggal'], 8,2);
+  $r['tanggal'] = strftime( "%A %d %B %Y", mktime(0,0,0,$bulan,$tanggal,$tahun));
+
+  $sql2 = "SELECT nota_beli_product.id_product, product.jenis, nota_beli_product.quantity, nota_beli_product.total 
+  FROM nota_beli_product INNER JOIN product ON nota_beli_product.id_product = product.id 
+  WHERE nota_beli_product.id_nota_beli = ?";
+
+  $stmt2 = $conn->prepare($sql2);
+  $stmt2->bind_param("s",$id);
+  $stmt2->execute();
+  $product=[];
+  $result2 = $stmt2->get_result();
+  if ($result2->num_rows > 0) {
+          while($r2=mysqli_fetch_assoc($result2))
+          {
+            array_push($product,$r2);
+          }
   }
-  $arr=["result"=>"success","data"=>$data];
+  $r["produk"]=$product;  
+  $arr=["result"=>"success","data"=>$r];
 } else {
   $arr=["result"=>"error","message"=>"sql error: $sql"];
 }
