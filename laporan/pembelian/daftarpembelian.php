04885fc732b97d6a82720db30bd39404c76d7514
nigelwilliam16 <s160419017@student.ubaya.ac.id>
2023-03-09 11:19:26 +0700

    daftar pembelian

--- laporan/pembelian/daftarpembelian.php
+++ laporan/pembelian/daftarpembelian.php
@@ -9,12 +9,13 @@ if($conn->connect_error) {
 setlocale(LC_ALL, 'IND');
 
 extract($_POST);
-
 $sql = "SELECT nota_beli.id, nota_beli.tanggal, nota_beli.waktu, SUM(nota_beli_product.quantity) AS jumlah_barang,
-SUM(nota_beli_product.total) AS total_pembelian, nota_beli.foto, nota_beli.id_cabang, cabang.nama_cabang FROM nota_beli INNER JOIN nota_beli_product 
-ON nota_beli.id = nota_beli_product.id_nota_beli INNER JOIN cabang ON nota_beli.id_cabang = cabang.id GROUP BY nota_beli.id";
+SUM(nota_beli_product.total) AS total_pembelian, nota_beli.foto, nota_beli.id_cabang, cabang.nama_cabang, nota_beli.username, account.nama_depan, account.nama_belakang FROM nota_beli INNER JOIN nota_beli_product 
+ON nota_beli.id = nota_beli_product.id_nota_beli INNER JOIN cabang ON nota_beli.id_cabang = cabang.id 
+INNER JOIN account ON nota_beli.username = account.username WHERE nota_beli.tanggal BETWEEN ? AND ? GROUP BY nota_beli.id;";
 
 $stmt = $conn->prepare($sql);
+$stmt->bind_param("ss",$startdate,$enddate);
 $stmt->execute();
 $result = $stmt->get_result();
 $data=[];
@@ -23,7 +24,7 @@ if($result->num_rows > 0) {
     $tahun = substr($r['tanggal'], 0,4);
     $bulan = substr($r['tanggal'], 5,2);
     $tanggal = substr($r['tanggal'], 8,2);
-    $r['tanggal'] = strftime( "%A %d %B %Y", mktime(0,0,0,$bulan,$tanggal,$tahun));
+    $r['tanggal'] = strftime( "%A, %d %B %Y", mktime(0,0,0,$bulan,$tanggal,$tahun));
     array_push($data,$r);
   }
   $arr=["result"=>"success","data"=>$data];
