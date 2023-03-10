a4a05a8120a38f131ae3b8a8d39e858a19dbb3ed
nigelwilliam16 <s160419017@student.ubaya.ac.id>
2023-03-10 11:55:22 +0700

    detailpembelian

--- laporan/pembelian/detailpembelian.php
+++ laporan/pembelian/detailpembelian.php
@@ -10,9 +10,10 @@ setlocale(LC_ALL, 'IND');
 
 extract($_POST);
 $sql = "SELECT nota_beli.id, nota_beli.tanggal, nota_beli.waktu, SUM(nota_beli_product.quantity) AS jumlah_barang,
-SUM(nota_beli_product.total) AS total_pembelian, nota_beli.foto, nota_beli.id_cabang, cabang.nama_cabang, nota_beli.username, account.nama_depan, account.nama_belakang FROM nota_beli INNER JOIN nota_beli_product 
-ON nota_beli.id = nota_beli_product.id_nota_beli INNER JOIN cabang ON nota_beli.id_cabang = cabang.id 
-INNER JOIN account ON nota_beli.username = account.username WHERE nota_beli_product.id_nota_beli = ?";
+SUM(nota_beli_product.total) AS total_pembelian, nota_beli.foto, nota_beli.id_cabang, cabang.nama_cabang, nota_beli.username, 
+account.nama_depan, account.nama_belakang FROM nota_beli INNER JOIN nota_beli_product ON nota_beli.id = nota_beli_product.id_nota_beli 
+INNER JOIN cabang ON nota_beli.id_cabang = cabang.id INNER JOIN account ON nota_beli.username = account.username 
+WHERE nota_beli_product.id_nota_beli = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s",$id);
 $stmt->execute();
@@ -24,7 +25,7 @@ if($result->num_rows > 0) {
   $tanggal = substr($r['tanggal'], 8,2);
   $r['tanggal'] = strftime( "%A %d %B %Y", mktime(0,0,0,$bulan,$tanggal,$tahun));
 
-  $sql2 = "SELECT nota_beli_product.id_product, product.jenis, nota_beli_product.quantity, nota_beli_product.total 
+  $sql2 = "SELECT nota_beli_product.id_product, product.jenis, nota_beli_product.quantity,product.harga, nota_beli_product.total 
   FROM nota_beli_product INNER JOIN product ON nota_beli_product.id_product = product.id 
   WHERE nota_beli_product.id_nota_beli = ?";
 
